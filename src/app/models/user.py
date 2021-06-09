c805a50274889cfbdd90ae54e9c204854ec74061
luolingchun <luolingchun@outlook.com>
2021-06-09 14:10:15 +0800

    delete relationship table primary_key

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -20,14 +20,14 @@ from . import Base, db
 
 user_role = db.Table(
     'user_role',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True)
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
 )
 
 role_permission = db.Table(
     'role_permission',
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True),
-    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'), primary_key=True)
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
+    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'))
 )
 
 
