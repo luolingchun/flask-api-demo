63223b665938eed999d07a0f92a2b0cf6960511d
luolingchun <luolingchun@outlook.com>
2021-12-08 17:37:09 +0800

    装饰器复制函数的__globals__和__extra_globals__

--- src/app/utils/jwt_tools.py
+++ src/app/utils/jwt_tools.py
@@ -43,6 +43,10 @@ def role_required(name, module, uuid):
             else:
                 raise AuthException(message='权限不足')
 
+        # 复制全局变量
+        wrapper.__extra_globals__ = func.__globals__
+        wrapper.__extra_globals__.update(**getattr(func, "__extra_globals__", {}))
+
         return wrapper
 
     return decorator
@@ -55,6 +59,10 @@ def login_required(func):
     def wrapper(*args, **kwargs):
         verify_jwt_in_request()
         return func(*args, **kwargs)
+
+    # 复制全局变量
+    wrapper.__extra_globals__ = func.__globals__
+    wrapper.__extra_globals__.update(**getattr(func, "__extra_globals__", {}))
 
     return wrapper
 
