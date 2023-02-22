74d36f8377652f04ae7c4f87b36340fad59ae489
luolingchun <luolingchun@outlook.com>
2023-02-22 11:37:35 +0800

    添加IdModel和FileModel

--- src/app/form/__init__.py
+++ src/app/form/__init__.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 # @Author  : llc
 # @Time    : 2020/5/4 17:11
-
+from flask_openapi3 import FileStorage
 from pydantic import BaseModel, Field
 
 
 class PageModel(BaseModel):
     page: int = Field(1, ge=1, description="页码")
     page_size: int = Field(15, ge=1, description="每页个数")
+
+
+class IdModel(BaseModel):
+    id: int = Field(..., description="ID")
+
+
+class FileModel(BaseModel):
+    file: FileStorage
 
 
 class JsonResponse(BaseModel):
